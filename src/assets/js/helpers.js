a2fe5bdb26738503c3ce34cafb042979ddb320e7
Prashant Jha <prashantjha.slg@gmail.com>
2023-05-18 12:02:59 +0530

    3rd commit

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -117,6 +117,7 @@ export default {
                     username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
                     credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
                     urls: [
+                        "stun:stun1.1.google.cpm:19302",
                         "turn:eu-turn4.xirsys.com:80?transport=udp",
                         "turn:eu-turn4.xirsys.com:3478?transport=tcp"
                     ]
